f231860366d066591bc6f9b88e07cf99cf41c900
ElectrixFox <69259529+ElectrixFox@users.noreply.github.com>
2026-04-06 17:56:00 +0100

    Proofing

--- 101ThingsToDoInLean/Test.lean
+++ 101ThingsToDoInLean/Test.lean
@@ -3,9 +3,22 @@ import Mathlib.Tactic
 example : 1 + 1 = 2 := by
   ring
 
-lemma corl1 (a b : ℤ) (hb : b ≠ 0) : Nonempty {x : ℤ ∣ (a - x * b) ∧ (a - x * b ≥ 0)} := by
+lemma corl1 (a b : ℤ) (hb : b ≠ 0) : Nonempty {y : ℤ | ∃ x : ℤ, y = a - x * b ∧ y ≥ 0} := by
+  sorry
+
+lemma corl2 (a b : ℤ) (hb : b ≠ 0) : ∃ (q : ℤ), a - q * b < b := by
   sorry
 
 theorem div_alg (a b : ℤ) (hb : b > 0) : ∃! q : ℤ, ∃! r : ℤ, a = b * q + r ∧ (0 ≤ r ∧ r < b) := by
+  have h1 := corl1
+  have h2 := corl2
+  specialize_all a
+  specialize_all b
+  have : b > 0 → b ≠ 0 := by
+    by_contra
+    simp at this
+    linarith
+
+  apply this at h1
 
   sorry
